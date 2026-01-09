d57c699907b8e470867238f3753293d2224cdb50
maik-avi <miguelgironda049@gmail.com>
2026-01-09 02:39:35 +0100

    ya está listo creo!

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,14 @@
 const sections = document.querySelectorAll('.link-section');
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, {
-  threshold: 0.4
-});
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  },
+  { threshold: 0.4 }
+);
 
 sections.forEach(section => observer.observe(section));
